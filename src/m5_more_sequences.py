538959562b7995874c586dbccc22269f69ea5f35
lcopland18 <coplanlm@rose-hulman.edu>
2019-03-28 08:59:14 -0400

    Objects and Functions

--- src/m5_more_sequences.py
+++ src/m5_more_sequences.py
@@ -77,7 +77,7 @@ def sum_radii(circles):
       :rtype: int | float
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # Note: No fair using "slices" on ANY of these problems,
@@ -156,10 +156,16 @@ def count_last_n_odds(integers, n):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
 
+    count = 0
+    for k in range(len(integers)-1,len(integers)-n-1,-1):
+        if integers[k] % 2 == 1:
+            count += 1
+
+    return count
 
 ###############################################################################
 # Some problems iterate (loop) through PART of the sequence,
@@ -231,10 +237,15 @@ def index_of_first_negative(numbers):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
 
+    for k in range(len(numbers)):
+        if numbers[k] < 0:
+            return k
+
+    return -1
 
 def run_test_contains_an_a():
     """ Tests the   contains_an_a   function. """
@@ -289,7 +300,7 @@ def contains_an_a(s):
       :rtype: bool
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ###########################################################################
@@ -303,6 +314,11 @@ def contains_an_a(s):
     #   No fair using the   count   or   find   string methods.
     # -------------------------------------------------------------------------
 
+    for k in range(len(s)):
+        if s[k] == "a":
+            return True
+
+    return False
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
